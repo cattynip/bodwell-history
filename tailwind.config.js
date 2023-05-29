dec8a935366eec09c5bcbbec00be7c2ed82024e4
cattynip <cattynip.cattynip@gmail.com>
2023-05-29 16:10:15 -0700

    config: default settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+const tailwindCSSConfig = {
+  content: ["./pages/**/*.{jsx,tsx}", "./components/**/*.{jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        by: "#FFD601",
+        bp: "#5447B8",
       },
     },
   },
   plugins: [],
-}
+};
+
+module.exports = tailwindCSSConfig;
