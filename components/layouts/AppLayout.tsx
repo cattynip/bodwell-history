e51027381cbb7d6e029c81017eb5222c129f67ba
cattynip <cattynip.cattynip@gmail.com>
2023-06-05 20:10:23 -0700

    feat(config): add meta tags in head

--- components/layouts/AppLayout.tsx
+++ components/layouts/AppLayout.tsx
@@ -11,6 +11,62 @@ const AppLayout = ({ children }: IAppLayout) => {
         <title>Bodwell History</title>
         <meta name="author" content="Cattynip <cattynip.cattynip@gmail.com>" />
         <meta name="viewport" content="width=device-width" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          data-hid="twitter:title"
+          name="twitter:title"
+          property="twitter:title"
+          content="Bodwell History"
+        />
+        <meta
+          data-hid="twitter:description"
+          name="twitter:description"
+          property="twitter:description"
+          content="Explore Bodwell High School through historical events!"
+        />
+        <meta
+          data-hid="twitter:image"
+          name="twitter:image"
+          property="twitter:image"
+          content="https://bodwell-history.vercel.app/logo.png"
+        />
+        <meta
+          data-hid="twitter:image:alt"
+          name="twitter:image:alt"
+          property="twitter:image:alt"
+          content="Bodwell History"
+        />
+        <meta
+          data-hid="og:title"
+          name="og:title"
+          property="og:title"
+          content="Bodwell History"
+        />
+        <meta
+          data-hid="og:description"
+          name="og:description"
+          property="og:description"
+          content="Explore Bodwell High School through historical events!"
+        />
+        <meta
+          data-hid="og:image"
+          name="og:image"
+          property="og:image"
+          content="https://bodwell-history.vercel.app/logo.png"
+        />
+        <meta
+          data-hid="og:image:secure_url"
+          name="og:image:secure_url"
+          property="og:image:secure_url"
+          content="https://bodwell-history.vercel.app/logo.png"
+        />
+        <meta
+          data-hid="og:image:alt"
+          name="og:image:alt"
+          property="og:image:alt"
+          content="Bodwell History"
+        />
       </Head>
 
       <div className="max-w-2xl p-3 mx-auto w-full h-[100vh]">{children}</div>
