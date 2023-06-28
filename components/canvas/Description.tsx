da63d0e045706fa6d30b986530969c0726d76753
cattynip <cattynip.cattynip@gmail.com>
2023-06-27 21:37:44 -0700

    refactor(components): implement the description and texts components

--- components/canvas/Description.tsx
+++ components/canvas/Description.tsx
@@ -1,26 +1,85 @@
-import { useEffect, useRef } from "react";
-import Texts, { ICallback, TextInfo } from "./Texts";
+import { useEffect } from "react";
+import Texts, { ICallback, defaultWaitingTimes } from "./Texts";
+import { AnimatePresence } from "framer-motion";
+import { LeonSansOptions } from "@nindaff/leonsans";
+import userLangInfo from "@components/languageProvider";
+import { motion } from "framer-motion";
+
+// type TBackgroundColourType = "black" | "white" | "blurred";
+// type TTextColourType = "white" | "black";
+type TCanvasShowingStyle = "quotes" | "description";
 
 interface IComponentProps {
-  text: TextInfo[];
+  text: string[];
+  canvasShowingStyle: TCanvasShowingStyle;
 }
 
-const Description = ({ text, ...props }: IComponentProps & ICallback) => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+const QuoteStyle: LeonSansOptions = {
+  color: ["#FFFFFF"],
+  size: 25,
+  weight: 300,
+  align: "center",
+  maxWidth: 1
+};
+
+const DescriptionStyle: LeonSansOptions = {
+  color: ["#DDDDDD"],
+  size: 15,
+  weight: 300,
+  align: "right",
+  maxWidth: 600
+};
+
+const Description = ({
+  text,
+  canvasShowingStyle,
+  ...props
+}: IComponentProps & ICallback) => {
+  const isLangEng = userLangInfo.userLang === "en";
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
+    if (isLangEng) {
+      const globalStyle: LeonSansOptions =
+        canvasShowingStyle === "quotes" ? QuoteStyle : DescriptionStyle;
 
-    const Text = new Texts(canvas, ctx, text, props);
+      const Text = new Texts({
+        content: text,
+        globalStyle,
+        timeInfo: defaultWaitingTimes,
+        callbacks: props
+      });
 
-    Text.startAnimation();
+      Text.startAnimation();
+    }
+
     /* eslint-disable react-hooks/exhaustive-deps */
   }, []);
 
-  return <canvas ref={canvasRef} className="z-20 rounded-xl bg-black" />;
+  return (
+    <div
+      className="w-full overflow-hidden rounded-md bg-black p-3 text-center text-white shadow-2xl"
+      id="parent"
+    >
+      <div id="leonsans">
+        <div id="sample">
+          <AnimatePresence>
+            <motion.div
+              initial={{
+                opacity: 1
+              }}
+              animate={{
+                opacity: 1
+              }}
+            >
+              {text.map((value, idx) => (
+                <p key={idx}>{value}</p>
+              ))}
+            </motion.div>
+          </AnimatePresence>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Description;
--- components/canvas/Texts.ts
+++ components/canvas/Texts.ts
@@ -1,7 +1,8 @@
 import anime from "animejs";
 import LeonSans, { LeonSansOptions } from "@nindaff/leonsans";
+import { PositionCoor } from "./GlowCanvas";
 
-type DeleteMethodT = "once" | "erasing" | "opacity";
+type CallbackFnT = () => void;
 
 interface IWaitingTimes<T = number> {
   delay: T;
@@ -10,77 +11,115 @@ interface IWaitingTimes<T = number> {
   deleting: T;
 }
 
-export interface TextInfo {
-  text: string;
-  deleteMethod?: DeleteMethodT;
-  timeInfo?: IWaitingTimes;
-  style?: LeonSansOptions;
+export const defaultWaitingTimes: IWaitingTimes = {
+  delay: 1000,
+  showing: 1000,
+  writing: 1000,
+  deleting: 1000
+};
+
+export interface ICallback {
+  whenStarted?: CallbackFnT;
+  everyWriting?: CallbackFnT;
+  whenCompleted?: CallbackFnT;
 }
 
-/*
- * TODO: Make it accept just a single paragraphs
- * TODO: Develop the different ways of deleting texts.
- */
+interface ITextProps {
+  content: string[];
+  globalStyle: LeonSansOptions;
+  timeInfo: IWaitingTimes;
+  callbacks?: ICallback;
+}
 
 class Texts {
+  private parentElement: HTMLDivElement;
+  private mainElement: HTMLDivElement;
   private canvas: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D;
   private pixelRatio: number;
   protected animation?: number;
-  private content: TextInfo[];
-  private currentContent: TextInfo;
+  private content: string[];
+  private currentContent: string;
   private currentLeon: LeonSans;
-  private defaultTimeInfo: IWaitingTimes;
+  private globalStyle: LeonSansOptions;
+  private timeInfo: IWaitingTimes;
+  private callbacks?: ICallback;
+  private position: PositionCoor;
 
-  constructor(
-    canvas: HTMLCanvasElement,
-    ctx: CanvasRenderingContext2D,
-    content: TextInfo[]
-  ) {
-    this.canvas = canvas;
-    this.ctx = ctx;
+  constructor({ content, globalStyle, timeInfo, callbacks }: ITextProps) {
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d")!;
+    this.parentElement = document.querySelector(`#parent`)!;
+    this.mainElement = this.parentElement.querySelector("#leonsans")!;
     this.content = content;
-    this.defaultTimeInfo = {
-      delay: 500,
-      showing: 1000,
-      writing: 1000,
-      deleting: 1000
-    };
+
+    this.globalStyle = globalStyle;
+    this.timeInfo = timeInfo;
 
     this.pixelRatio = 2;
-    canvas.width = 800 * this.pixelRatio;
-    canvas.height = 400 * this.pixelRatio;
-    canvas.style.width = `800px`;
-    canvas.style.height = `400px`;
-    ctx.scale(this.pixelRatio, this.pixelRatio);
 
+    this.canvas.width = this.mainElement.clientWidth * this.pixelRatio;
+    this.canvas.height = this.mainElement.clientHeight * this.pixelRatio;
+    this.canvas.style.width = `${this.mainElement.clientWidth}px`;
+    this.canvas.style.height = `${this.mainElement.clientHeight}px`;
+    this.ctx.scale(this.pixelRatio, this.pixelRatio);
     this.currentContent = this.content[0];
-    this.currentLeon = new LeonSans(this.getLeonSansOption());
+    this.currentLeon = new LeonSans({
+      text: this.currentContent,
+      ...this.globalStyle
+    });
+
+    this.callbacks = callbacks;
+    this.position = this.getPosition();
+
+    this.attach();
 
     this.animate();
+  }
+
+  private attach() {
+    const exists = this.parentElement.querySelector("canvas");
+
+    if (!exists) {
+      this.mainElement.appendChild(this.canvas);
+      const sample = this.mainElement.querySelector("#sample");
+      sample?.remove();
+    }
   }
 
   public async startAnimation() {
     this.maintainingTransparent();
 
+    this.callbacks?.whenStarted && this.callbacks.whenStarted();
+
     for (let i = 0; i < this.content.length; i++) {
       this.currentContent = this.content[i];
-      this.currentLeon = new LeonSans(this.getLeonSansOption());
+      this.currentLeon = new LeonSans({
+        text: this.currentContent,
+        ...this.globalStyle
+      });
       this.maintainingTransparent();
-      const timeInfo: IWaitingTimes = this.currentContent.timeInfo
-        ? this.currentContent.timeInfo
-        : this.defaultTimeInfo;
 
-      await this.waitFor(timeInfo.delay);
+      this.position = this.getPosition();
 
-      this.processWriting(timeInfo.writing);
+      await this.waitFor(this.timeInfo.delay);
 
-      await this.waitFor(timeInfo.writing + timeInfo.showing);
+      this.processWriting(this.timeInfo.writing);
 
-      this.processDeleting(timeInfo.deleting);
+      await this.waitFor(this.timeInfo.writing + this.timeInfo.showing);
 
-      await this.waitFor(timeInfo.deleting);
+      const isLast = this.content.length - 1 === i;
+
+      this.callbacks?.everyWriting && this.callbacks.everyWriting();
+
+      if (isLast) return;
+
+      this.processDeleting(this.timeInfo.deleting);
+
+      await this.waitFor(this.timeInfo.deleting);
     }
+
+    this.callbacks?.whenCompleted && this.callbacks.whenCompleted();
   }
 
   private async waitFor(t: number) {
@@ -88,7 +127,7 @@ class Texts {
   }
 
   private processWriting(duration: number) {
-    for (let i = 0; i < this.currentContent["text"].length; i++) {
+    for (let i = 0; i < this.currentContent.length; i++) {
       let letterInfo = {
         prop: 0
       };
@@ -113,7 +152,7 @@ class Texts {
   }
 
   private processDeleting(duration: number) {
-    for (let i = 0; i < this.currentContent["text"].length; i++) {
+    for (let i = 0; i < this.currentContent.length; i++) {
       let letterInfo = {
         prop: 1
       };
@@ -137,10 +176,23 @@ class Texts {
     }
   }
 
+  private getPosition(): PositionCoor {
+    const textAlign = this.globalStyle ? this.globalStyle.align : "center";
+
+    return textAlign === "center"
+      ? {
+          x: this.canvas.width / 4 - this.currentLeon.rect.w / 2,
+          y: this.canvas.height / 4 - this.currentLeon.rect.h / 2
+        }
+      : textAlign === "right"
+      ? { x: 0, y: 0 }
+      : { x: 0, y: 0 };
+  }
+
   private maintainingTransparent() {
     if (!this.currentContent) return;
 
-    for (let i = 0; i < this.currentContent["text"].length; i++) {
+    for (let i = 0; i < this.currentContent.length; i++) {
       if (!this.currentLeon.data[i]) return;
 
       // @ts-ignore
@@ -148,32 +200,10 @@ class Texts {
     }
   }
 
-  private getLeonSansOption(): LeonSansOptions {
-    const defaultLeonSansOptions: LeonSansOptions = {
-      color: ["#FFFFFF"],
-      size: 25,
-      weight: 300,
-      align: "center",
-      maxWidth: 100
-    };
-
-    const style: LeonSansOptions = this.currentContent.style
-      ? this.currentContent.style
-      : defaultLeonSansOptions;
-
-    return {
-      text: this.currentContent.text,
-      ...style
-    };
-  }
-
   public animate() {
     this.clear();
 
-    this.currentLeon.position(
-      this.canvas.width / (2 * this.pixelRatio) - this.currentLeon.rect.w / 2,
-      this.canvas.height / (2 * this.pixelRatio) - this.currentLeon.rect.h / 2
-    );
+    this.currentLeon.position(this.position.x, this.position.y);
     this.currentLeon.draw(this.ctx);
 
     requestAnimationFrame(this.animate.bind(this));
